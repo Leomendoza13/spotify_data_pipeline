93135f1b6d3fd79acbda071529b27060859ef61a
Leomendoza13 <leo.mendoza@epita.fr>
2024-11-06 16:23:45 +0100

    variables.tf refactored

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,28 @@
 variable "project_id" {
-  description = "ID of project"
+  description = "ID of the Google Cloud project"
   type        = string
 }
 
 variable "ssh_user" {
-  description = "SSH User"
+  description = "SSH user for connecting to instances"
   type        = string
+  default     = "default-ssh-user"
 }
 
 variable "ssh_pub_key_path" {
-  description = "SSH path key"
+  description = "Path to the SSH public key"
   type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "source_folder" {
-  description = "Path to the local folder containing the files to upload"
+  description = "Path to the local folder containing files to upload to the instance"
   type        = string
+  default     = "../dags/"
 }
 
 variable "ids_path" {
-  description = "Path of ids.json for SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET"
+  description = "Path to the ids.json file for SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET"
   type        = string
+  default     = "../config/"
 }
